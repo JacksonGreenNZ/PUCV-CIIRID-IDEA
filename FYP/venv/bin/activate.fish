f49e81d355aeb77721205a073414a40ca0e13210
Jackson Green <jacksonuea@gmail.com>
2026-02-11 15:37:29 +1300

    sorting out dependency issues

--- FYP/venv/bin/activate.fish
+++ FYP/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /root/spaceborne-rfi/PUCV-CIIRID-IDEA/FYP/FYP/venv
+set -gx VIRTUAL_ENV /root/spaceborne-rfi/PUCV-CIIRID-IDEA/FYP/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
