dd548876abf0c431d27117b01e15e0fa785330a7
Jackson Green <jacksonuea@gmail.com>
2026-03-01 14:53:15 +1300

    added tle data manager to sopp_runner, changed visualisation so that the 0-90 on the sky plot is alt/az rather than from zenith

--- FYP/core/sopp_runner.py
+++ FYP/core/sopp_runner.py
@@ -5,8 +5,8 @@ from config import (
     LATITUDE, LONGITUDE, ELEVATION_M,
     RA_HOURS, DEC_DEGREES,
     TIME_BEGIN, TIME_END,
-    FREQUENCY_HZ, TLE_FILE,
-    CONCURRENCY_LEVEL
+    FREQUENCY_HZ,
+    CONCURRENCY_LEVEL, DATA_TYPE
 )
 
 class SOPPRunner:
@@ -18,6 +18,21 @@ class SOPPRunner:
         self.beam_model = beam_model
         self.config = self._build_config()
 
+    @staticmethod
+    def select_data(group):
+        import os
+        from skyfield.api import load
+
+        max_days = 7.0
+        filename = f"data/{group}.tle"
+        url = f"https://celestrak.org/NORAD/elements/gp.php?GROUP={group}&FORMAT=tle"
+
+        if not os.path.exists(filename) or load.days_old(filename) >= max_days:
+            print(f"Downloading TLEs for {group}...")
+            load.download(url, filename=filename)
+
+        return filename
+
     def _build_config(self):
         """
         Builds SOPP configuration using hardcoded parameters for testing and
@@ -25,6 +40,8 @@ class SOPPRunner:
         """
         frequency_mhz = FREQUENCY_HZ / 1e6
 
+        tle_file = self.select_data(DATA_TYPE)
+        
         return (
             ConfigurationBuilder()
             .set_facility(
@@ -38,9 +55,11 @@ class SOPPRunner:
             .set_time_window(begin=TIME_BEGIN, end=TIME_END)
             .set_frequency_range(bandwidth=10, frequency=frequency_mhz)
             .set_observation_target(f"{DEC_DEGREES}d", right_ascension=f"{RA_HOURS}h")
-            .set_satellites(tle_file=TLE_FILE)
+            .set_satellites(tle_file=tle_file)
             .build()
         )
+
+    
 
     def run(self):
         """
